f3372bf4a197934a42da4b14bedbd678a9144706
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-17 20:48:16 +0000

    reset search form after the user clicks on search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 var apiKey = "f142d21647feca195690187a6be73e98";
 
-
 var submitBtn = document.getElementById("search-button");
 submitBtn.addEventListener('click', function(event) {
-    event.preventDefault();
     var searchInput = document.getElementById("search-input").value;
+    event.preventDefault();
     console.log(searchInput);
     localStorage.setItem("City Name", searchInput);
     getWeatherData();
+    var searchForm = document.getElementById("search-form");
+    searchForm.reset();
 });
 
 
