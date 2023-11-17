3359b4c0df8b3cdf921246d2f9a6458c7ee1ce13
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-17 19:06:56 +0000

    use the geoQuery url to fetch data about London to check the function is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
 var apiKey = "f142d21647feca195690187a6be73e98";
-var lat = 51.5073219;
-var lon = -0.1276474;
 var cityName = "London";
-var limit = 1;
-var forecast = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-console.log(forecast);
-var geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=" + limit + "&appid=" + apiKey;
+
+var geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
 console.log(geoQuery);
+
+fetch(geoQuery)
+    .then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+
+        var lat = data[0].lat;
+        console.log(lat);
+
+        var lon = data[0].lon;
+        console.log(lon);
+
+        var cityName = data[0].name;
+        console.log(cityName);
+
+        var forecast = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        console.log(forecast);
+    });
+
+ 
