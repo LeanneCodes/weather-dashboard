e1677ce510a95faf6136b279df6a6fce75fbe8f5
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-17 20:21:35 +0000

    add cityName and fetch queries to getWeatherData function and call it in the submit button event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,48 @@
 var apiKey = "f142d21647feca195690187a6be73e98";
 
-function getCityName(event) {
-    event.preventDefault();
-    var searchInput = document.getElementById("search-input").value;
-    var cityName = searchInput;
-    console.log(cityName);
-    return cityName;
-}
 
 var submitBtn = document.getElementById("search-button");
-submitBtn.addEventListener('click', getCityName);
+submitBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    var searchInput = document.getElementById("search-input").value;
+    console.log(searchInput);
+    localStorage.setItem("City Name", searchInput);
+    getWeatherData();
+});
 
 
-var geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
-console.log(geoQuery);
+function getWeatherData() {
+    var cityName = localStorage.getItem("City Name");
 
-fetch(geoQuery)
-    .then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data);
+    var geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+    console.log(geoQuery);
 
-        var lat = data[0].lat;
-        console.log(lat);
+    fetch(geoQuery)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
 
-        var lon = data[0].lon;
-        console.log(lon);
+            var lat = data[0].lat;
+            console.log(lat);
 
-        var cityName = data[0].name;
-        console.log(cityName);
+            var lon = data[0].lon;
+            console.log(lon);
 
-        var forecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        console.log(forecast);
+            var cityName = data[0].name;
+            console.log(cityName);
 
-        fetch(forecast)
-            .then(function(response) {
-                return response.json();
-            }).then(function(data){
-                console.log(data);
-            })
-    });
+            var forecast = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+            console.log(forecast);
+
+            fetch(forecast)
+                .then(function(response) {
+                    return response.json();
+                }).then(function(data){
+                    console.log(data);
+                })
+        });
+}
+
+
 
