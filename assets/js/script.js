377fbf500a4881070b9eeba70a1fca1b7696d355
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-17 21:43:23 +0000

    add five day forecast object to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,55 @@ function getWeatherData() {
                     var today = {
                         date: dayjs().$d,
                         city: data.city.name,
+                        icon: data.list[0].weather[0].icon,
                         temperature: data.list[0].main.temp,
                         wind: data.list[0].wind.speed,
                         humidity: data.list[0].main.humidity
-                    }
+                    };
+                    
+                    localStorage.setItem(cityName, JSON.stringify(today));
                     console.log(today);
+
+                    var fiveDayForecast = {
+                        day1: {
+                            date: data.list[0].dt_txt,
+                            icon: data.list[0].weather[0].icon,
+                            temperature: data.list[0].main.temp,
+                            wind: data.list[0].wind.speed,
+                            humidity: data.list[0].main.humidity
+                        },
+                        day2: {
+                            date: data.list[8].dt_txt,
+                            icon: data.list[8].weather[0].icon,
+                            temperature: data.list[8].main.temp,
+                            wind: data.list[8].wind.speed,
+                            humidity: data.list[8].main.humidity
+                        },
+                        day3: {
+                            date: data.list[16].dt_txt,
+                            icon: data.list[16].weather[0].icon,
+                            temperature: data.list[16].main.temp,
+                            wind: data.list[16].wind.speed,
+                            humidity: data.list[16].main.humidity
+                        },
+                        day4: {
+                            date: data.list[24].dt_txt,
+                            icon: data.list[24].weather[0].icon,
+                            temperature: data.list[24].main.temp,
+                            wind: data.list[24].wind.speed,
+                            humidity: data.list[24].main.humidity
+                        },
+                        day5: {
+                            date: data.list[32].dt_txt,
+                            icon: data.list[32].weather[0].icon,
+                            temperature: data.list[32].main.temp,
+                            wind: data.list[32].wind.speed,
+                            humidity: data.list[32].main.humidity
+                        }
+                    };
+
+                    localStorage.setItem("5 Day Forecast", JSON.stringify(fiveDayForecast));
+                    console.log(fiveDayForecast);
                 })
         });
 }
