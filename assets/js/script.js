2c5954abf0010f05dba6f8b3cf9d955126f5931d
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-18 16:43:35 +0000

    only creates one button for each city searched for based on the button value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// when the window loads, previous stored weather data and buttons appear on screen
 document.addEventListener("DOMContentLoaded", function() {
     var recentWeatherData = localStorage.getItem("City Name");
 
@@ -52,17 +53,20 @@ searchHistory.addEventListener('click', function(event) {
 });
 
 
+// only create a new button if it doesn't currently exist
 function createButton(cityName) {
-    // cityName = localStorage.getItem("City Name");
-    var cityBtn = document.createElement("button");
-    cityBtn.textContent = cityName;
-    cityBtn.classList.add("btn", "cityBtn");
-    cityBtn.setAttribute("value", cityName);
-    searchHistory.append(cityBtn);
-    console.log(cityName);
+    if (!document.querySelector(`.cityBtn[value="${cityName}"]`)) {
+        var cityBtn = document.createElement("button");
+        cityBtn.textContent = cityName;
+        cityBtn.classList.add("btn", "cityBtn");
+        cityBtn.setAttribute("value", cityName);
+        searchHistory.append(cityBtn);
+        console.log(cityName);
+    }
 };
 
 
+// displays the button if it doesn't have City Name or 5 Day Forecase appended to it
 function displayButtons() {
     var cities = Object.keys(localStorage);
     cities.forEach(function(city) {
@@ -72,6 +76,12 @@ function displayButtons() {
     })
 }
 
+
+// fetches the weather data using the city name and api key first
+// then creates variables for lat and lon using the first query data
+// then passes those variables into the openweathermap url to get today's data
+// and then we call the 5 day forecast api using the same lat and lon values for that city
+// both sets of data are stored in objects and saved to local storage
 function getWeatherData(cityName) {
     var geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
     console.log(geoQuery);
@@ -123,39 +133,39 @@ function getWeatherData(cityName) {
 
                     var fiveDayForecast = {
                         day1: {
-                            date: data.list[0].dt_txt,
-                            icon: data.list[0].weather[0].icon,
-                            temperature: data.list[0].main.temp,
-                            wind: data.list[0].wind.speed,
-                            humidity: data.list[0].main.humidity
+                            date: data.list[4].dt_txt,
+                            icon: data.list[4].weather[0].icon,
+                            temperature: data.list[4].main.temp,
+                            wind: data.list[4].wind.speed,
+                            humidity: data.list[4].main.humidity
                         },
                         day2: {
-                            date: data.list[8].dt_txt,
-                            icon: data.list[8].weather[0].icon,
-                            temperature: data.list[8].main.temp,
-                            wind: data.list[8].wind.speed,
-                            humidity: data.list[8].main.humidity
+                            date: data.list[12].dt_txt,
+                            icon: data.list[12].weather[0].icon,
+                            temperature: data.list[12].main.temp,
+                            wind: data.list[12].wind.speed,
+                            humidity: data.list[12].main.humidity
                         },
                         day3: {
-                            date: data.list[16].dt_txt,
-                            icon: data.list[16].weather[0].icon,
-                            temperature: data.list[16].main.temp,
-                            wind: data.list[16].wind.speed,
-                            humidity: data.list[16].main.humidity
+                            date: data.list[20].dt_txt,
+                            icon: data.list[20].weather[0].icon,
+                            temperature: data.list[20].main.temp,
+                            wind: data.list[20].wind.speed,
+                            humidity: data.list[20].main.humidity
                         },
                         day4: {
-                            date: data.list[24].dt_txt,
-                            icon: data.list[24].weather[0].icon,
-                            temperature: data.list[24].main.temp,
-                            wind: data.list[24].wind.speed,
-                            humidity: data.list[24].main.humidity
+                            date: data.list[28].dt_txt,
+                            icon: data.list[28].weather[0].icon,
+                            temperature: data.list[28].main.temp,
+                            wind: data.list[28].wind.speed,
+                            humidity: data.list[28].main.humidity
                         },
                         day5: {
-                            date: data.list[32].dt_txt,
-                            icon: data.list[32].weather[0].icon,
-                            temperature: data.list[32].main.temp,
-                            wind: data.list[32].wind.speed,
-                            humidity: data.list[32].main.humidity
+                            date: data.list[36].dt_txt,
+                            icon: data.list[36].weather[0].icon,
+                            temperature: data.list[36].main.temp,
+                            wind: data.list[36].wind.speed,
+                            humidity: data.list[36].main.humidity
                         }
                     };
 
@@ -166,6 +176,7 @@ function getWeatherData(cityName) {
 };
 
 
+// display data on screen using DOM manipulation
 function displayWeatherData(cityName) {
     // create the two weather sections of the dashboard
     var todaySection = document.getElementById("today");
@@ -228,6 +239,7 @@ function displayWeatherData(cityName) {
     var forecastArray = Object.values(forecastData);
     console.log(forecastArray);
 
+    // iterate through the array and append the values on screen dynamically
     for (var i = 0; i < forecastArray.length; i++) {
         var divEl = document.createElement("div");
         divEl.setAttribute("class", "col");
@@ -256,5 +268,5 @@ function displayWeatherData(cityName) {
         divEl.append(humidityData);
     };
 
-    displayButtons();
+    displayButtons(); // buttons are created and displayed after every search
 };
